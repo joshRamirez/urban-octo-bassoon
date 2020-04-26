005065b0515e1379b5b7a62fe7f78dc19672a938
Joshua Ramirez <josh20615@gmail.com>
2020-04-25 19:32:18 -0700

    Add junit. Add fake test

---
+++ calculator/src/test/java/UtilTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class UtilTest {
+    @Test
+    public void testConvertStringToRpn() {
+        assertTrue(true);
+    }
+
+}
