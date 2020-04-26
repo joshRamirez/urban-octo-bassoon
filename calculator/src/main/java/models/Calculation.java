009e63b3f77b2264a7ca8e6b44417da66be7e03d
Joshua Ramirez <josh20615@gmail.com>
2020-04-25 19:17:33 -0700

    Calculation object which will house input, conversion, and output

---
+++ calculator/src/main/java/models/Calculation.java
@@ -0,0 +1,31 @@
+package models;
+
+public class Calculation {
+    private String input;
+    private String rpn;
+    private String output;
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public void setOutput(String output) {
+        this.output = output;
+    }
+
+    public String getRpn() {
+        return rpn;
+    }
+
+    public void setRpn(String rpn) {
+        this.rpn = rpn;
+    }
+}
