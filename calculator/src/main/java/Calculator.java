aca6bc747b64ab7c8e61a2be62782da44ea7ed34
Joshua Ramirez <josh20615@gmail.com>
2020-04-26 18:37:46 -0700

    Updates to main method

--- calculator/src/main/java/Calculator.java
+++ calculator/src/main/java/Calculator.java
@@ -1,13 +1,19 @@
 import models.Calculation;
+import util.ConversionUtil;
+import util.SolutionUtil;
 
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        Calculation calculation = new Calculation();
+        ConversionUtil conversionUtil = new ConversionUtil();
+        SolutionUtil solutionUtil = new SolutionUtil();
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter your equation: ");
+        System.out.println("Enter your equation. Only use parenthesis, base 10 positive integers and only the following operators: +,-,*,/");
+        Calculation calculation = new Calculation();
         calculation.setInput(input.nextLine());
-
+        calculation.setRpn(conversionUtil.convertStringToRpn(calculation.getInput()));
+        calculation.setOutput(solutionUtil.solveEquation(calculation.getRpn()));
+        System.out.println("Solution: " + calculation.getOutput());
     }
 }
